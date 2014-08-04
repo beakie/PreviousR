c4fa9a44eac594ee23d28e3e6e52ceb310b5e86a
beakie <garyewen@gmail.com>
2014-08-04 20:23:45 +0000

    random checkin

--- common-imatrix.h
+++ common-imatrix.h
@@ -9,7 +9,6 @@ struct IMatrix
 {
 public:
     virtual void set(const TVALUE value) = 0;
-    virtual void set(const TVALUE x1y1, const TVALUE x1y2, const TVALUE x1y3, const TVALUE x1y4, const TVALUE x2y1, const TVALUE x2y2, const TVALUE x2y3, const TVALUE x2y4, const TVALUE x3y1, const TVALUE x3y2, const TVALUE x3y3, const TVALUE x3y4, const TVALUE x4y1, const TVALUE x4y2, const TVALUE x4y3, const TVALUE x4y4) = 0;
     virtual void set(const TINDEX x, const TINDEX y, const TVALUE value) = 0;
     virtual void set(const TIMPLEMENTATION &matrix) = 0;
     virtual TVALUE get(const TINDEX x, const TINDEX y) = 0;
